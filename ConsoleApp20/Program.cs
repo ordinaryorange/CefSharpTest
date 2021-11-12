c76666edc17ba48bf70555babb721d5d05e7fd4e
Austin Brandis <abrandis@envirotechmonitoring.com.au>
2021-11-12 14:02:01 +1030

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,19 @@
+﻿using CefSharp.OffScreen;
+using System;
+using System.Threading;
+using CefSharp;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cef.Initialize(new CefSettings());
+            var browser = new ChromiumWebBrowser("http://www.google.com");
+            Thread.Sleep(2000); //wait for init
+            browser.ShowDevTools();
+            Console.ReadLine();
+        }
+    }
+}
